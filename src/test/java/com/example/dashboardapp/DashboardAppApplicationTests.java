3a9b6e036bf40d542ecc8d0b3b8bd06632c90ee2
DAVNGU99 <david.h.nguyen1999@gmail.com>
2025-12-18 19:35:35 +0100

    init java setup

---
+++ src/test/java/com/example/dashboardapp/DashboardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dashboardapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
